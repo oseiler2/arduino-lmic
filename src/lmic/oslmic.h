231aacb4ef6583d2fff9088f50a0c03a580b0840
Oliver Seiler <oliver@seiler.kiwi>
2024-03-30 09:39:56 +1300

    fix os_timeToNextTimeCriticalJob declaration

--- src/lmic/oslmic.h
+++ src/lmic/oslmic.h
@@ -215,6 +215,8 @@ u1_t os_getBattLevel (void);
 #ifndef os_queryTimeCriticalJobs
 //! Return non-zero if any jobs are scheduled between now and now+time.
 bit_t os_queryTimeCriticalJobs(ostime_t time);
+#endif
+#ifndef os_timeToNextTimeCriticalJob
 // return the time to the next scheduled job, or ostime_t_max if no job is scheduled
 ostime_t os_timeToNextTimeCriticalJob();
 #endif
